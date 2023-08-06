f579cd7b454479c97e7ac4c04b718d743395e5b2
Shariful Islam <sharifsharifulislam061@gmail.com>
2023-08-06 21:52:31 +0600

    [Done] cors express.json done

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
-import express, { Application } from 'express'
+import express, { Application, Response,Request } from 'express'
+import cors from 'cors'
 const app:Application=express()
+
 const port=process.env.PORT||5000
-app.get("/",(req,res)=>{
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.get("/",(req:Request,res:Response)=>{
     res.send("Welcome to university management site!!!")
 })
 export default app;
